09934aefe0de73823866c04187bf51b4cdc3652e
krishnan165 <84275949+krishnan165@users.noreply.github.com>
2021-07-25 18:45:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+             var first_name="sri";
+             console.log(first_name);
+
+             var second_name="krishnan";
+             console.log(second_name);
+
+             var full_name=first_name.concat(second_name);
+             console.log(full_name);
+
+             var addtion="addtion : ";
+             console.log(addtion);
+
+             var  a=100;
+             console.log(a);
+
+             var  b=22;
+             console.log(b);
+
+             var add=a+b;
+             console.log(add);
+
+             var  multiplication="Multiplication : ";
+             console.log(multiplication);
+             var mn1=12;
+             console.log(mn1);
+             var mn2=4;
+             Console.log(mn2);
+             var multi=mn1*mn2;
+             console.log(multi);
+
+             var subtraction="Subtraction : ";
+             console.log(subtraction);
+             var sn1=10;
+             console.log(sn1);
+             var sn2=5;
+             console.log(sn2);
+             var sub=a-b;
+             console.log(sub);
+
+             var division="Division : ";
+             console.log(division);
+             var dn1=12;
+             console.log(dn1);
+             var dn2=2;
+             console.log(dn2);
+             var divi=dn1/dn2;
+             console.log(divi);
+
+
+             
